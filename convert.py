bc495e1ef49bd4054e4dfa1f520300b3719957e3
harryrocket01 <118760733+harryrocket01@users.noreply.github.com>
2024-02-20 15:47:31 +0000

    Add files via upload

--- convert.py
+++ convert.py
@@ -12,7 +12,14 @@ import scipy.interpolate as interpolate
 class BatteryConvert:
 
     def __init__(self):
+
+        # file to convert
         self.root = "HVBatteryCurrent_1Lap.xlsx"
+        # name to save
+        self.save_name = "Wave_1"
+        # number of points
+        self.points = 100
+
         self.raw = None
         self.sampled = None
         column_names = [
@@ -24,12 +31,12 @@ class BatteryConvert:
             "F",
             "G",
             "H",
-        ]  # AC START, AC END,STAR FREQ, END FREQ , AC START ANGLE, DC START, DC END,SQUENCE POINT IN TIME
+        ]
+        # AC START, AC END,STAR FREQ, END FREQ , AC START ANGLE, DC START, DC END,SQUENCE POINT IN TIME
 
         self.saved = pd.DataFrame(0, columns=column_names, index=range(99))
 
     def load(self):
-
         self.raw = pd.read_excel(self.root)
         print(self.raw.head())  # Display the first few rows of the DataFrame
         shape = self.raw.shape
@@ -40,7 +47,7 @@ class BatteryConvert:
 
     def convert(self, method="1"):
         signal_np = self.raw.to_numpy()
-        decimation_factor = len(signal_np) // 100
+        decimation_factor = len(signal_np) // self.points
 
         # Decimation
         if method == "1":
@@ -49,26 +56,26 @@ class BatteryConvert:
 
         # Interpolation
         elif method == "2":
-            points_per_segment = len(signal_np) // 100
-            segments = signal_np[: points_per_segment * 100].reshape(
-                (100, points_per_segment, 2)
+            points_per_segment = len(signal_np) // self.points
+            segments = signal_np[: points_per_segment * self.points].reshape(
+                (self.points, points_per_segment, 2)
             )
             downsampled_current = np.max(segments[:, :, 1], axis=1)
             downsampled_time = segments[
-                np.arange(100), np.argmax(segments[:, :, 1], axis=1), 0
+                np.arange(self.points), np.argmax(segments[:, :, 1], axis=1), 0
             ]
             downsampled_signal = np.column_stack(
                 (downsampled_time, downsampled_current)
             )
 
         else:  # For method "3"
-            points_per_segment = len(signal_np) // 100
+            points_per_segment = len(signal_np) // self.points
             segments = signal_np[: points_per_segment * 100].reshape(
-                (100, points_per_segment, 2)
+                (self.points, points_per_segment, 2)
             )
             downsampled_current = np.mean(segments[:, :, 1], axis=1)
             downsampled_time = segments[
-                np.arange(100), np.argmax(segments[:, :, 1], axis=1), 0
+                np.arange(self.points), np.argmax(segments[:, :, 1], axis=1), 0
             ]
             downsampled_signal = np.column_stack(
                 (downsampled_time, downsampled_current)
@@ -84,7 +91,7 @@ class BatteryConvert:
 
     def save(self):
         for index, row in self.sampled.iterrows():
-            if index == 100:
+            if index == self.points:
                 break
             current_f = round(float(row["Current"]), 4)
             current_g = round(float(self.sampled.at[index + 1, "Current"]), 4)
@@ -94,7 +101,7 @@ class BatteryConvert:
 
         print(self.saved)
         self.saved.to_csv(
-            "HMI_FILES/Generate_Wave.csv", sep=";", decimal=".", index=False
+            f"HMI_FILES/{self.save_name}.csv", sep=";", decimal=".", index=False
         )
         self.saved[["F", "G"]].plot()
         plt.show()
